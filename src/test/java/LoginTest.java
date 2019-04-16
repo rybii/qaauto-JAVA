d63441eff56bc66bfaed477fd0be3537f8cee13a
Volodymyr Rybii <vmrybiy@gmail.com>
2019-04-16 10:09:30 +0300

    Home Work Lesson 3

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,7 +12,14 @@ public class LoginTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\vryb\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
-        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up ");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up ");
+        WebElement sessionKey = driver.findElement(By.xpath("//input[@name='session_key']"));
+        sessionKey.sendKeys("semencement2@gmail.com");
+        WebElement sessionPassword = driver.findElement(By.xpath("//input[@name='session_password']"));
+        sessionPassword.sendKeys("Semen1002");
+        WebElement signIn = driver.findElement(By.xpath("//input[@class='login submit-button']"));
+        signIn.click();
+        Assert.assertEquals(driver.getTitle(), "LinkedIn");
 
     }
 }
